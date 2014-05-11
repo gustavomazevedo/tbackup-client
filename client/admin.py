cb4f33af8f73250f324243739f2037acafc716d3
Gustavo Azevedo <gustavo.m.azevedo@gmail.com>
2014-05-11 16:40:45 -0300

    register models to admin

--- client/admin.py
+++ client/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from models import (Destination, Origin, Configuration, WebServer)
+
+admin.site.register(Destination)
+admin.site.register(Origin)
+admin.site.register(Configuration)
+admin.site.register(WebServer)
