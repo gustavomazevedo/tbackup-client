155f3c340b757f0f626fb2295079c033b2858419
Gustavo Azevedo <gustavo.m.azevedo@gmail.com>
2014-07-27 16:18:04 -0300

    commented old tests

--- client/tests.py
+++ client/tests.py
@@ -21,40 +21,40 @@ PATH='/Users/gustavo/'
 # Create your tests here.
 
 class DestinationCase(TestCase):
-
-    def setUp(self):
-        ws = WebServer.objects.create(
-            name=settings.WEBSERVER_NAME,
-            url=settings.WEBSERVER_URL,
-            api_url=settings.WEBSERVER_API_URL,
-            api_version=settings.WEBSERVER_API_VERSION
-        )
-        
-        o = Origin.objects.create(
-            name='Gustavo',
-        )
-        
-    def test_loadDestination(self):
-        Destination.update()
-        for d in Destination.objects.all():
-            print name
-        
-    def test_register(self):
-        Destination.update()
-        Origin.register()
-        
-        WebServer.get().backup(
-            {
-                ''
-            }
-        )
-        
-        
-    def test_backup(self):
-        Destination.update()
-        Origin.register()
-        
-        fn2 = os.path.join(PATH, 'virtualenv-1.11.4.tar.gz')
-        django_file = File(open(fn2, 'rb'))
-        
-        
+    pass
+    #def setUp(self):
+    #    ws = WebServer.objects.create(
+    #        name=settings.WEBSERVER_NAME,
+    #        url=settings.WEBSERVER_URL,
+    #        api_url=settings.WEBSERVER_API_URL,
+    #        api_version=settings.WEBSERVER_API_VERSION
+    #    )
+    #    
+    #    o = Origin.objects.create(
+    #        name='Gustavo',
+    #    )
+    #    
+    #def test_loadDestination(self):
+    #    Destination.update()
+    #    for d in Destination.objects.all():
+    #        print name
+    #    
+    #def test_register(self):
+    #    Destination.update()
+    #    Origin.register()
+    #    
+    #    WebServer.get().backup(
+    #        {
+    #            ''
+    #        }
+    #    )
+    #    
+    #    
+    #def test_backup(self):
+    #    Destination.update()
+    #    Origin.register()
+    #    
+    #    fn2 = os.path.join(PATH, 'virtualenv-1.11.4.tar.gz')
+    #    django_file = File(open(fn2, 'rb'))
+    #    
+    #    
