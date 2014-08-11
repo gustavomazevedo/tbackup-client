7dcecc308923b4f05fbd0cdd09a2de0d691502f4
Gustavo Azevedo <gustavo.m.azevedo@gmail.com>
2014-08-10 22:05:10 -0300

    fixing conf.settings

--- client/models.py
+++ client/models.py
@@ -7,7 +7,7 @@ from dateutil                 import rrule
 from django.db                import models
 from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse
-from django.conf.settings     import STATIC_URL
+#from django.conf.settings     import STATIC_URL
 from .conf.settings           import (
     DATETIME_FORMAT,
     GET,
@@ -15,7 +15,8 @@ from .conf.settings           import (
     WEBSERVER_NAME,
     WEBSERVER_URL,
     WEBSERVER_API_URL,
-    WEBSERVER_API_VERSION
+    WEBSERVER_API_VERSION,
+    STATIC_URL   
 )
 #from .constants               import GET, POST
 from .functions               import json_request, normalize_time
