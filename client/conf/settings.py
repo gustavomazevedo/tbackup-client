b6dd6a0435f5d054514a7e12b9ee424632c35d94
Gustavo Azevedo <gustavo.m.azevedo@gmail.com>
2014-08-10 21:25:32 -0300

    Added conf dir and settings.py inside it. Started migrating constants and configurations to conf/settings.py

---
+++ client/conf/settings.py
@@ -0,0 +1,12 @@
+
+from django.conf import settings
+
+POST = 'POST'
+GET  = 'GET'
+
+DATETIME_FORMAT = getattr(settings, 'DATETIME_FORMAT', '%Y%m%d%H%M')
+
+WEBSERVER_NAME = getattr(settings, 'WEBSERVER_NAME','WebServer')
+WEBSERVER_URL = getattr(settings, 'WEBSERVER_URL','http://127.0.0.1:7000')
+WEBSERVER_API_URL = getattr(settings, 'WEBSERVER_API_URL','/api')
+WEBSERVER_API_VERSION = getattr(settings, 'WEBSERVER_API_VERSION','v1')
