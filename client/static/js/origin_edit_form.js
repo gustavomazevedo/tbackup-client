ab3f604b1f661e3fa92f77450ab62658e725f578
Gustavo Azevedo <gustavo.m.azevedo@gmail.com>
2014-11-28 14:56:07 -0200

    strategy to disable name field and still capture value in the server side: inject an enabled hidden input with same name and value

--- client/static/js/origin_edit_form.js
+++ client/static/js/origin_edit_form.js
@@ -7,7 +7,6 @@ django.jQuery(document).ready(function($) {
     $('.field-new_password2').hide()
     $('input[name=_continue]').remove()
     $('input[name=_save]').val('Atualizar')
-    
     $('#id_change_password').change(function() {
         if (this.checked) {
             $('.field-new_password1').show()
@@ -17,5 +16,13 @@ django.jQuery(document).ready(function($) {
             $('.field-new_password1').hide()
             $('.field-new_password2').hide()
         }
+    });
+    
+    //hidden field so django captures value for disabled field
+    $('#id_name').each(function() {
+        $(this).after(
+            '<input type="hidden" name="'  + $(this).attr('name') +
+                               '" value="' + $(this).val() +
+            '" />');
     });
 });
